4e8ad4bccfaa0d185c36adf448f5994478772984
LinShunC <sdon0006@student.monash.edu>
2019-09-07 18:40:01 +1000

    Update getRelated.php

--- CompleteV1/phpAction/getRelated.php
+++ CompleteV1/phpAction/getRelated.php
@@ -2,13 +2,13 @@
 include "db.php";
 
 // get the q parameter from URL
-
-    $sql = "SELECT * FROM product 
-    INNER JOIN accessorysupport 
-    ON product.productId=supportedPID
-    RIGHT JOIN pictures
+$pid = $_REQUEST["q"];
+        $sql = "SELECT * FROM accessorysupport
+    INNER JOIN product
+    on accessorysupport.productId=product.productId
+    INNER JOIN pictures
     ON pictures.productId=accessorysupport.productId
-    where product.productId=3
+    where supportedPID='.$pid.'
     limit 2";
 $results = mysqli_query($connection,$sql);
 
@@ -18,9 +18,11 @@ if(mysqli_num_rows($results)>0){
         $prod_name = $row["pName"];
         $prod_pic1 = $row["link1"];
           $prod_Pri = $row["price"];
+        $productid=$row["productId"];
         echo '<div class="relatedBox">
                    <p>'.$prod_name.'</p>
-                   <img src="'.$prod_pic1.'" alt="img1" style="height:100px;">
+                   <a href="productDetails.php?pid='.$productid.'">
+                   <img src="'.$prod_pic1.'" alt="img1" style="height:100px;"></a>
                    <p>Details</p>
                    <p>price: $'.$prod_Pri.'</p>
              </div>
