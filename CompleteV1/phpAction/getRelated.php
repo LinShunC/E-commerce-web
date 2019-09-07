315b43ac791479b69a33747d024e8695ee255fe3
jguzaa <jongjetw@dyn-49-127-146-172.its.monash.edu.au>
2019-09-07 16:21:38 +1000

    completeV1

---
+++ CompleteV1/phpAction/getRelated.php
@@ -0,0 +1,27 @@
+<?php
+include "db.php";
+
+// get the q parameter from URL
+
+$sql = "SELECT * FROM product INNER JOIN pictures
+order by rand() limit 2";
+$results = mysqli_query($connection,$sql);
+
+if(mysqli_num_rows($results)>0){
+
+    while ($row = mysqli_fetch_array($results)){
+        $prod_name = $row["pName"];
+        $prod_pic1 = $row["link1"];
+          $prod_Pri = $row["price"];
+    
+
+        echo '<div class="relatedBox">
+                   <p>'.$prod_name.'</p>
+                   <img src="'.$prod_pic1.'" alt="img1" style="height:100px;">
+                   <p>Details</p>
+                   <p>price: $'.$prod_Pri.'</p>
+             </div>
+            ';
+    }
+}
+?>
