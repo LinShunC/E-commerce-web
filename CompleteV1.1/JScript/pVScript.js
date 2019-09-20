7c13a2cf2f6642d8b886346e307bd7ee03fc915f
jguzaa <jongjetw@Jongjets-MacBook-Pro.local>
2019-09-20 12:26:44 +1000

    1.1

---
+++ CompleteV1.1/JScript/pVScript.js
@@ -0,0 +1,59 @@
+function loadCat(catId) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "phpAction/catSelectAction.php?id="+catId, true);
+    xhttp.send();   
+}
+
+function loadKw(keyword) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "phpAction/kwLookUp.php?kw="+keyword, true);
+    xhttp.send();   
+}
+
+function loadAcc(accId) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "phpAction/accSelectAction.php?id="+accId, true);
+    xhttp.send();   
+}
+
+function loadBreadCrumCat(catId) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("breadcrumb").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "phpAction/breadcrumCatAction.php?id="+catId, true);
+    xhttp.send();   
+}
+
+function loadBreadCrumAcc(accId) {
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("breadcrumb").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "phpAction/breadcrumAccAction.php?id="+accId, true);
+    xhttp.send();   
+}
---
+++ CompleteV1.1/productsView.php
@@ -0,0 +1,173 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Products</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+        <script> 
+            $(function(){
+                $("#includedNav").load("navbar.php"); 
+            });
+            $(function(){
+                $("#includedFooter").load("footer.html"); 
+            });
+        </script> 
+
+        <script src="JScript/pVScript.js"></script>
+
+        <link rel="stylesheet" type="text/css" href="css/pVStyle.css">
+        <link rel="stylesheet" type="text/css" href="css/navbarStyle.css">
+
+        <?php
+
+        if (isset($_GET["keyword"])){
+            $keyword = $_GET["keyword"];
+            echo "
+                    <script> 
+                        $(function(){
+                            loadKw('$keyword'); //load product which related kw
+                            loadBreadCrumCat(0);
+                        });
+                    </script> 
+
+                ";
+            
+        }
+        else if (isset($_REQUEST["id"])){
+            $catId = $_REQUEST["id"];
+
+            echo "
+
+                    <script> 
+                        $(function(){
+                            loadCat($catId);
+                            loadBreadCrumCat($catId);
+                        });
+                    </script> 
+
+                ";
+
+        } else if (isset($_REQUEST["aid"])){
+            $accId = $_REQUEST["aid"];
+
+            echo "
+
+                    <script> 
+                        $(function(){
+                            loadAcc($accId);
+                            loadBreadCrumAcc($accId);
+                        });
+                    </script> 
+
+                ";
+
+        } else {
+
+            echo "
+                    <script> 
+                        $(function(){
+                            loadCat(0); //load random 9 products
+                            loadBreadCrumCat(0);
+                        });
+                    </script> 
+
+                ";
+
+        }
+        
+        //Searching things
+        if (isset($_REQUEST["keyword"])){
+            
+        }
+
+        ?>
+
+
+    </head>
+
+    <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+        <div id="includedNav"></div>
+        
+        <ol class="breadcrumb" id="breadcrumb" style="margin-bottom:0px">
+              
+        </ol>
+
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="Pictures/promotion1.jpg" alt="New York">
+                    <div class="carousel-caption">
+                        <h3>Promotion1</h3>
+                    </div>
+                </div>
+
+                <div class="item" >
+                    <img src="Pictures/promotion1.jpg" alt="Chicago">
+                    <div class="carousel-caption">
+                        <h3>Promotion2</h3>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="Pictures/promotion1.jpg" alt="Los Angeles">
+                    <div class="carousel-caption">
+                        <h3>Promotion3</h3>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <div class = "mainContent row">
+
+
+
+            <div class="row col-sm-2" id="sideList">
+
+                <div id="catMenu" class="list-group" id="list-tab" role="tablist">
+                    <?php 
+                    include "phpAction/catListAction.php";
+                    ?>
+                </div>
+
+            </div>
+
+
+            <div class="row col-sm-10" id="content"></div>
+
+        </div>
+
+        <div id="includedFooter"></div>
+
+    </body>
+</html>
